07af98998cdac39c773f59d8743656ea2f29a2bd
Tuganbai Shynbolat <Tuganbai Shynbolat>
2017-05-20 09:39:00 +0600

    20

--- Task2/Task2/Form1.cs
+++ Task2/Task2/Form1.cs
@@ -12,9 +12,30 @@ namespace Task2
 {
     public partial class Form1 : Form
     {
+        static int a = 0;
+        
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            {
+                if (textBox1.Text== "a")
+                {
+                    b.Text = 1;
+                }
+                if (textBox1.Text==)
+            }
         }
     }
 }
